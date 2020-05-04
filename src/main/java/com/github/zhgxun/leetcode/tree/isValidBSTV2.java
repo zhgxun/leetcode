9d8774d9d57855e9dc484a118e9084991a1dc064
zhangguangxun <zhangguangxun@xiaomi.com>
2020-05-04 18:36:27 +0800

    验证二叉搜索树的两种方式

--- src/main/java/com/github/zhgxun/leetcode/tree/isValidBSTV2.java
+++ src/main/java/com/github/zhgxun/leetcode/tree/isValidBSTV2.java
@@ -9,22 +9,27 @@ import com.github.zhgxun.leetcode.TreeNode;
  */
 public class isValidBSTV2 {
 
+    // 二叉搜索树相当中序遍历是递增有序排序数组
+    private long preValue = Long.MIN_VALUE;
+
+    // 中序遍历的方式验证是否为二叉搜索树
     public boolean isValidBST(TreeNode root) {
+        return inOrder(root);
+    }
+
+    // 借助中序遍历的特性
+    public boolean inOrder(TreeNode root) {
         if (root == null) {
             return true;
         }
-
-        TreeNode maxLeft = root.left;
-        while (maxLeft != null && maxLeft.right != null) {
-            maxLeft = maxLeft.right;
+        boolean result = inOrder(root.left);
+        if (!result) {
+            return false;
         }
-        TreeNode minRight = root.right;
-        while (minRight != null && minRight.left != null) {
-            minRight = minRight.left;
+        if (root.val <= preValue) {
+            return false;
         }
-
-        boolean result = (maxLeft == null || maxLeft.val < root.val) && (minRight == null || minRight.val > root.val);
-
-        return result && isValidBST(root.left) && isValidBST(root.right);
+        preValue = root.val;
+        return inOrder(root.right);
     }
 }

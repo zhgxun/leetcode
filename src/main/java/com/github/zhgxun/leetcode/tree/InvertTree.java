40c536f95224c6692d997f0085a087ea884bec03
zhangguangxun <zhangguangxun@xiaomi.com>
2020-05-06 00:01:12 +0800

    反转二叉树

---
+++ src/main/java/com/github/zhgxun/leetcode/tree/InvertTree.java
@@ -0,0 +1,45 @@
+package com.github.zhgxun.leetcode.tree;
+
+import com.github.zhgxun.leetcode.TreeNode;
+import com.github.zhgxun.leetcode.TreeNodeUtil;
+
+/**
+ * 226. 翻转二叉树
+ * <p>
+ * https://leetcode-cn.com/problems/invert-binary-tree/
+ * <p>
+ * 翻转一棵二叉树。
+ */
+public class InvertTree {
+
+    public static void main(String[] args) {
+        TreeNode tree = new TreeNode(4);
+        tree.left = new TreeNode(2);
+        tree.left.left = new TreeNode(1);
+        tree.left.right = new TreeNode(3);
+        tree.right = new TreeNode(7);
+        tree.right.left = new TreeNode(6);
+        tree.right.right = new TreeNode(9);
+
+        System.out.println(TreeNodeUtil.preOrderTraversal(tree));
+        new InvertTree().invertTree(tree);
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.left == null && root.right == null) {
+            return root;
+        }
+
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
